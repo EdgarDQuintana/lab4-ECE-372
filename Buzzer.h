33cff1ffab99d08f80384c6f938f4c628396e9c6
EdmundSheah <44382870+EdmundSheah@users.noreply.github.com>
2018-10-29 15:32:05 -0700

    Create Buzzer.h

---
+++ Buzzer.h
@@ -0,0 +1,8 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+void initBUZZER();
+void turnOnBUZZER(unsigned char data);// I am not sure we need the input data(unsigned char data) 
+void turnOffBUZZER();// not sure if this function is required 
+
+#endif
