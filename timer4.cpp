34a03f60becd638af110825a0b16df764bfd0904
Emanuel Inacio <emanuelinacio1@hotmail.com>
2018-10-29 14:44:59 -0700

    Update timer4.cpp

--- timer4.cpp
+++ timer4.cpp
@@ -3,6 +3,6 @@
 void initTimer4(){
   TCCR4A |= (1 << WGM40);//correct PWM mode(frequency): WGM40 sets as 1
   TCCR4A &= ~(1 << WGM41);//correct PWM mode(frequency): WGM41 sets as 0
-  TCCR4B &= ~(1 << WGM42);//correct PWM mode(frequency): WGM40 sets as 0
-  TCCR4B |= (1 << WGM43);//correct PWM mode(frequency): WGM40 sets as 1
+  TCCR4B &= ~(1 << WGM42);//correct PWM mode(frequency): WGM42 sets as 0
+  TCCR4B |= (1 << WGM43);//correct PWM mode(frequency): WGM43 sets as 1
 }
